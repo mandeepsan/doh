7e5b62ea5c8fd0a62e494fafe8e09b4adff2ecde
Mandeep Sangwan <sangwanmandeep2009@gmail.com>
2025-05-10 14:07:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: 0BSD
 
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://dns.nextdns.io/d25f13/'
+const dohjson = 'https://dns.nextdns.io/d25f13/'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 const path = ''; // default allow all, must start with '/' if specified, eg. "/dns-query"
